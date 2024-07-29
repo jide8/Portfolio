a1a1a595066b38777e9f9c853adf4f72504dedbc
jide8 <jideoketayo08@gmail.com>
2024-07-29 16:28:36 +0100

    fin

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -9,7 +9,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/OKETAYOJIDE.pdf';
+import CV from '../../assets/techiej.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
